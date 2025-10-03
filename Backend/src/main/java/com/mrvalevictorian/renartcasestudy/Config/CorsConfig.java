b6d6d97b96030c8f60b7e1b82542d40689a9b0d4
Onat Kaan Atılgan <onatkaanatilgan@gmail.com>
2025-10-03 16:26:18 +0300

    Revision

--- Backend/src/main/java/com/mrvalevictorian/renartcasestudy/Config/CorsConfig.java
+++ Backend/src/main/java/com/mrvalevictorian/renartcasestudy/Config/CorsConfig.java
@@ -6,13 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("http://localhost:5173", "https://renart-case-study-y9xg.vercel.app/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
+
