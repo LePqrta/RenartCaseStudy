83b3beb5b620789d9a979ac6de84c1cef2404a45
Onat Kaan Atılgan <onatkaanatilgan@gmail.com>
2025-10-03 23:31:28 +0300

    Updated CorsConfix.java for the new frontend path

--- Backend/src/main/java/com/mrvalevictorian/renartcasestudy/Config/CorsConfig.java
+++ Backend/src/main/java/com/mrvalevictorian/renartcasestudy/Config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://renart-case-study-y9xg.vercel.app")                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:5173", "https://renart-case-study-ipkb.vercel.app/")                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
 
