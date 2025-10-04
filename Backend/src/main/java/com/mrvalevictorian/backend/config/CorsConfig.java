02b99cf19819580f2d81260c56f20069c12b0ef2
Onat Kaan Atılgan <onatkaanatilgan@gmail.com>
2025-10-04 17:51:27 +0300

    Update CorsConfig.java

--- Backend/src/main/java/com/mrvalevictorian/backend/config/CorsConfig.java
+++ Backend/src/main/java/com/mrvalevictorian/backend/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://renart-case-study-ipkb.vercel.app/")
+                .allowedOrigins("http://localhost:*", "https://renart-case-study*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
